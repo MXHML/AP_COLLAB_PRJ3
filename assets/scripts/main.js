5a0e4dbcd8a4f95befc489c00315b4e67f4dd53b
MXHML <114451098+MXHML@users.noreply.github.com>
2023-01-17 09:36:59 -0600

    init

---
+++ assets/scripts/main.js
@@ -0,0 +1,14 @@
+let index=0;
+async function main(index){
+    image.src=animals[index].image
+
+    for(let x=index;x<animals.length;x++){
+        
+    }
+}
+
+
+
+animals=[
+    {name:'dog',weight:'199',}
+]
