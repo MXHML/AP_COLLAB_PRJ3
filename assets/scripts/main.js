1f05bb4c62c0343c163a6c30a36e110a7cb72f33
MXHML <114451098+MXHML@users.noreply.github.com>
2023-01-17 09:57:29 -0600

    code

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,14 +1,15 @@
+const currentAnimalImage = document.getElementById("animal-image")
+
 let index=0;
-async function main(index){
-    image.src=animals[index].image
 
-    for(let x=index;x<animals.length;x++){
-        
-    }
-}
-
-
+$(document).ready(async ()=>{
+    currentAnimalImage.src=animals[index].image_src
+})
 
 animals=[
-    {name:'dog',weight:'199',}
+    {animal:'alpaca',weight:'110',height:'2.7-3.2ft',lifespan:'18-24 Years',fact:'Alpacas have one heart',image_src:'assets/images/alpaca.jpeg'}
 ]
+
+async function sleep(ms){
+    return new Promise(resolve=>setTimeout(resolve,ms))
+}
